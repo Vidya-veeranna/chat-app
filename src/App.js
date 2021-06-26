43a9a01462aea3356d9f41a3654f31f36500e44c
vidya-coder <vidyagowda191@gmail.com>
2021-06-26 13:55:44 +0530

    setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import 'rsuite/dist/styles/rsuite-default.css';
+import './styles/main.scss';
+
+function App() {
+  return <div className="App">hello</div>;
+}
+
+export default App;
