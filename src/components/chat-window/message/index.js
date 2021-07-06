7912078b49921c80662986c106f251c5ad7f5465
vidya-coder <vidyagowda191@gmail.com>
2021-07-06 20:24:47 +0530

    chat message displaying

--- src/components/chat-window/message/index.js
+++ src/components/chat-window/message/index.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router';
+import { database } from '../../../misc/Firebase';
+import { transformToArrayId } from '../../../misc/Helper';
 
 const Message = () => {
-  return <div>df</div>;
+  const { chatId } = useParams();
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    const messageRef = database.ref('/messages');
+
+    messageRef
+      .orderByChild('roomId')
+      .equalTo(chatId)
+      .on('value', snap => {
+        const data = transformToArrayId(snap.val());
+        setMessage(data);
+      });
+    return () => {
+      messageRef.off('value');
+    };
+  }, [chatId]);
+  return <div>{message}</div>;
 };
 
 export default Message;
