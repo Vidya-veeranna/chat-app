5f60c5f486221db330a78d26ad104e58d4c127e7
vidya-coder <vidyagowda191@gmail.com>
2021-07-07 15:26:01 +0530

    displayed messages

--- src/components/chat-window/message/index.js
+++ src/components/chat-window/message/index.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
 import { database } from '../../../misc/Firebase';
 import { transformToArrayId } from '../../../misc/Helper';
+import MessageItem from './MessageItem';
 
 const Message = () => {
   const { chatId } = useParams();
   const [message, setMessage] = useState(null);
 
+  const isChatEmpty = message && message.length === 0;
+  const canShowMessages = message && message.length > 0;
   useEffect(() => {
     const messageRef = database.ref('/messages');
 
@@ -21,7 +24,13 @@ const Message = () => {
       messageRef.off('value');
     };
   }, [chatId]);
-  return <div>{message}</div>;
+  return (
+    <ul className="msg-list custom-scroll">
+      {isChatEmpty && <li>No messages let</li>}
+      {canShowMessages &&
+        message.map(msg => <MessageItem key={msg.id} message={msg} />)}
+    </ul>
+  );
 };
 
 export default Message;
